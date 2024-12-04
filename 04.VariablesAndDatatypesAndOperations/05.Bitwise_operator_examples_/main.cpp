9a7365f0404445c5fb0adaac2aaef8a22375fded
Daniel Gakwaya <wayadn88@gmail.com>
2024-12-04 10:19:33 +0300

    Add code for examples on bitwise operator

---
+++ 04.VariablesAndDatatypesAndOperations/05.Bitwise_operator_examples_/main.cpp
@@ -0,0 +1,16 @@
+/*
+    . Bitwise operator examples: 
+        . Masks
+        . Packing color information.
+*/
+
+import utilities;
+
+int main(){
+
+    //use_options_v0(false, false, true, true, true, false, true, false);
+    //fmt::println("----------------");
+    //use_options_v1(mask_bit_2 | mask_bit_3 | mask_bit_4 | mask_bit_6);
+    pack_color_information();
+    
+}
