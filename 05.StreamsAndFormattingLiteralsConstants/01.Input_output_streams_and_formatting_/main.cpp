c2f8b8ac3576a98f22b57246c046ea0985d607b5
Daniel Gakwaya <wayadn88@gmail.com>
2024-12-06 10:53:51 +0300

    Add code for the format library

---
+++ 05.StreamsAndFormattingLiteralsConstants/01.Input_output_streams_and_formatting_/main.cpp
@@ -0,0 +1,19 @@
+/*
+    . Topics: 
+        . Input and output streams
+        . Stream formatting
+        . The format library
+        . Literals
+        . Constants
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <ios>
+
+import utilities;
+
+int main(){
+    input_output_iostream();
+    output_formatting();
+}
---
+++ 05.StreamsAndFormattingLiteralsConstants/02.Format_library_/main.cpp
@@ -0,0 +1,15 @@
+/*
+    . Explore the format library in C++20 and C++23
+*/
+
+//#include <format>
+//#include <print>
+#include <iostream>
+#include <fmt/format.h>
+
+import utilities;
+
+int main(){
+    //standard_format_facilities();
+    external_format_facilities();
+}
