77bb272972d2afe52db909fd8d90ded265275019
Daniel Gakwaya <wayadn88@gmail.com>
2024-12-24 09:42:55 +0300

    Add code for fortune teller

---
+++ 07.Sequences/06.Fortune_tellter_/main.cpp
@@ -0,0 +1,16 @@
+/*
+    . Fortune tellet project
+*/
+
+#include <fmt/format.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <iostream>
+
+import utilities;
+
+int main(){
+    //fortune_teller_good();
+    fortune_teller_bad();
+}
