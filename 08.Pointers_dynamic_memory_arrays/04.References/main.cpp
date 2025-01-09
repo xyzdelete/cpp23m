30c50e80dc8e1a3a9f6b9d67477d1f033398131a
Daniel Gakwaya <wayadn88@gmail.com>
2025-01-09 11:23:40 +0300

    Add code for references

---
+++ 08.Pointers_dynamic_memory_arrays/04.References/main.cpp
@@ -0,0 +1,19 @@
+/*
+    . Exploring references
+    . Topics: 
+        . Declaring and using references
+        . Compare pointers to references
+        . References and const
+        . references with range based for loop
+
+*/
+
+import utilities;
+
+int main(){
+    //declaring_and_using_references();
+    //comparing_pointers_and_references();
+    //references_and_const();
+    ref_with_range_based_for_loop();
+    
+}
